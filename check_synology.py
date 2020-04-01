0b826e021d7ea0c7a4dbe61e0dd44d00e4be957d
Michael Naylor <34126775+mjnaylor8@users.noreply.github.com>
2020-04-01 16:49:47 +0100

    Update check_synology.py

--- check_synology.py
+++ check_synology.py
@@ -1,52 +1,71 @@
-from pysnmp.hlapi import *
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import argparse
 import sys
 import math
+import easysnmp
 
 AUTHOR = "Frederic Werner"
-VERSION = 0.1
+VERSION = 0.2
+#0.2  Changes made to allow SNMP v2 access. Michael Naylor - 31 March 2020
 
 parser = argparse.ArgumentParser()
 parser.add_argument("hostname", help="the hostname", type=str)
-parser.add_argument("username", help="the snmp user name", type=str)
-parser.add_argument("authkey", help="the auth key", type=str)
-parser.add_argument("privkey", help="the priv key", type=str)
-parser.add_argument("mode", help="the mode", type=str, choices=["load", "memory", "disk", "storage", "update", "status"])
+#parser.add_argument("username", help="the snmp user name", type=str)
+#parser.add_argument("authkey", help="the auth key", type=str)
+#parser.add_argument("privkey", help="the priv key", type=str)
+parser.add_argument("community", help="the community name", type=str)
+parser.add_argument("mode", help="the mode", type=str, choices=["load", "real_memory", "swap_memory", "disk", "storage", "update", "status"])
 parser.add_argument("-w", help="warning value for selected mode", type=int)
 parser.add_argument("-c", help="critical value for selected mode", type=int)
 args = parser.parse_args()
 
 hostname = args.hostname
-user_name = args.username
-auth_key = args.authkey
-priv_key = args.privkey
+#user_name = args.username
+#auth_key = args.authkey
+#priv_key = args.privkey
+community = args.community
 mode = args.mode
 warning = args.w
 critical = args.c
-
 state = 'OK'
 
-def snmpget(oid):
-    errorIndication, errorStatus, errorIndex, varBinds = next(
-        getCmd(SnmpEngine(),
-               UsmUserData(user_name, auth_key, priv_key, authProtocol=usmHMACMD5AuthProtocol, privProtocol=usmAesCfb128Protocol),
-               UdpTransportTarget((hostname, 161)),
-               ContextData(),
-               ObjectType(ObjectIdentity(oid)
-                #.addAsn1MibSource('file:///usr/share/snmp', 'http://mibs.snmplabs.com/asn1/@mib@')
-                ))
-    )
+try:
 
-    if errorIndication:
-        print(errorIndication)
-    elif errorStatus:
-        print('%s at %s' % (errorStatus.prettyPrint(),
-                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
-    else:
-        for varBind in varBinds:
-            answer = (' = '.join([x.prettyPrint() for x in varBind]))
-            #print(' = '.join([x.prettyPrint() for x in varBind]))
-            return x.prettyPrint()
+    session = easysnmp.Session(
+        hostname=hostname,
+        community=community,
+        version=2)
+#        version=3,
+#        security_level="auth_with_privacy",
+#        security_level="auth_without_privacy",
+#        security_username=user_name,
+#        auth_password=auth_key,
+#        auth_protocol="MD5",
+#        privacy_password=priv_key,
+#        privacy_protocol="AES128")
+
+except easysnmp.EasySNMPError as e:
+    print(e)
+    exit(-1)
+
+def snmpget(oid):
+    try:
+        res = session.get(oid)
+        return res.value
+    except easysnmp.EasySNMPError as e:
+        print(e)
+
+# Walk the given OID and return all child OIDs as a list of tuples of OID and value
+def snmpwalk(oid):
+    res = []
+    try:
+        res = session.walk(oid)
+    except easysnmp.EasySNMPError as e:
+        print(e)
+    return res
+
 
 def exitCode():
     if state == 'OK':
@@ -59,6 +78,11 @@ def exitCode():
         sys.exit(3)
 
 if mode == 'load':
+    #set up variables for output and icinga performance
+    #see performance rules here https://icinga.com/docs/icinga1/latest/en/perfdata.html
+    output = ''
+    perfdata = ' | '
+    #get load data for 1, 5 and 15 mins
     load1 = str(float(snmpget('1.3.6.1.4.1.2021.10.1.5.1'))/100)
     load5 = str(float(snmpget('1.3.6.1.4.1.2021.10.1.5.2'))/100)
     load15 = str(float(snmpget('1.3.6.1.4.1.2021.10.1.5.3'))/100)
@@ -68,33 +92,82 @@ if mode == 'load':
     if critical and critical < int(math.ceil(float(load1))):
         state = 'CRITICAL'
 
-    print state + ' - load average: %s, %s, %s' % (load1, load5, load15), '| load1=%sc' % load1, 'load5=%sc' % load5, 'load15=%sc' % load15
+    output = (state + ' - Load average (1 min {:0.0f}% , 5 min {:0.0f}% , 15 min {:0.0f}% )'.format((float(load1)*100), (float(load5)*100), (float(load15)*100)))
+    perfdata += ('Load_1min={:0.1f}% Load_5min={:0.1f}% Load_15min={:0.1f}%'.format((float(load1)*100), (float(load5)*100), (float(load15)*100)))
+
+    #print(state + ' - load average (1 min, 5 min, 15 min): %s, %s, %s' % (load1, load5, load15), '| load1=%s' % load1, 'load5=%s' % load5, 'load15=%s' % load15)
+    #print(state + ' - Load average (1 min, 5 min, 15 min): {:0.0f}% {:0.0f}% {:0.0f}%'.format((float(load1)*100), (float(load5)*100), (float(load15)*100)))
+#          ' | load1=%s' % load1, 'load5=%s' % load5, 'load15=%s' % load15)
+    print(output, perfdata)
     exitCode()
 
-if mode == 'memory':
-    memory_total = float(snmpget('1.3.6.1.4.1.2021.4.5.0'))
-    memory_unused = float(snmpget('1.3.6.1.4.1.2021.4.6.0'))
-    memory_percent = 100 / memory_total * memory_unused
+if mode == 'real_memory':
+    #set up variables for output and icinga performance
+    output = ''
+    perfdata = '| '
+    #get memory data  for real memory (note some memory is reserved and so percentage not based on total physical memory)
+    #formatting of data changed to be similar to that of Synology Disk Manager software
+    memory_total_real = float(snmpget('1.3.6.1.4.1.2021.4.5.0'))
+    memory_free_real = float(snmpget('1.3.6.1.4.1.2021.4.6.0'))
+    memory_total_free = float(snmpget('1.3.6.1.4.1.2021.4.11.0'))
+    memory_shared = float(snmpget('1.3.6.1.4.1.2021.4.13.0'))
+    memory_buffer = float(snmpget('1.3.6.1.4.1.2021.4.14.0'))
+    memory_cached = float(snmpget('1.3.6.1.4.1.2021.4.15.0'))
+    memory_used_real = memory_total_real - memory_buffer - memory_cached - memory_free_real
+    memory_used_percent_real = memory_used_real / memory_total_real * 100
 
-    if warning and warning > int(memory_percent):
+#    print(memory_total_real / 1024)
+#    print('Free - ' + str(memory_total_free / 1024))
+#    print('Total Real - ' + str(memory_total_real / 1024))
+#    print('Total Free - ' + str(memory_free_real / 1024))
+#    print('Shared - ' + str(memory_shared / 1024))
+#    print('Buffer - ' + str(memory_buffer / 1024))
+#    print('Cached - '  + str(memory_cached / 1024))
+
+    if warning and int(memory_used_percent_real) > warning:
         state = 'WARNING'
-    if critical and critical > int(memory_percent):
+    if critical and int(memory_used_percent_real) > critical:
         state = 'CRITICAL'
 
-    print state + ' - {:0.1f}% '.format(memory_percent) + 'free ({0:0.1f} MB out of {1:0.1f} MB)'.format((memory_unused / 1024), (memory_total / 1024)), '|memory_total=%dc' % memory_total, 'memory_unused=%dc' % memory_unused , 'memory_percent=%d' % memory_percent + '%'
+    output = (state + ' - {:0.1f}% '.format(memory_used_percent_real) + 'Used ({0:0.1f} MB out of {1:0.1f} MB)'.format((memory_used_real / 1024), (memory_total_real / 1024)))
+    perfdata += ('Memory_Total={:0.1f}MB Memory_Used={:0.1f}MB Memory_Percent={:0.1f}%'.format((memory_total_real / 1024), (memory_used_real / 1024), memory_used_percent_real))
+    print (output, perfdata)
+    exitCode()
+
+if mode == 'swap_memory':
+    #set up variables for output and icinga performance
+    output = ''
+    perfdata = ' | '
+    #get memory data  for swap memory
+    memory_total_swap = float(snmpget('1.3.6.1.4.1.2021.4.3.0'))
+    memory_free_swap = float(snmpget('1.3.6.1.4.1.2021.4.4.0'))
+
+    memory_used_swap = memory_total_swap - memory_free_swap
+    memory_used_percent_swap = memory_used_swap / memory_total_swap * 100
+
+    if warning and int(memory_used_percent_swap) > warning:
+        state = 'WARNING'
+    if critical and int(memory_used_percent_swap) > critical:
+        state = 'CRITICAL'
+
+    output = (state + ' - {:0.1f}% '.format(memory_used_percent_swap) + 'Used ({0:0.1f} MB out of {1:0.1f} MB)'.format((memory_used_swap / 1024), (memory_total_swap / 1024)))
+    perfdata += ('Memory_Total={:0.1f}MB Memory_Used={:0.1f}MB Memory_Percent={:0.1f}%'.format((memory_total_swap / 1024), (memory_used_swap / 1024), memory_used_percent_swap))
+
+    print(output, perfdata)
+
     exitCode()
 
 if mode == 'disk':
     maxDisk = 0
     output = ''
-    perfdata = '|'
-    for i in range(0, 64, 1):
-        disk = snmpget('1.3.6.1.4.1.6574.2.1.1.2.' + str(i))
-        if disk.startswith("No Such Instance"):
-            break
-        maxDisk = maxDisk + 1
-    for i in range(0, maxDisk, 1):
-        disk_name = snmpget('1.3.6.1.4.1.6574.2.1.1.2.' + str(i))
+    perfdata = '| '
+    for item in snmpwalk('1.3.6.1.4.1.6574.2.1.1.2'):
+        i = item.oid.split('.')[-1]
+        #fetched some additonal items for interest
+        disk_name = item.value
+        disk_id = snmpget('1.3.6.1.4.1.6574.2.1.1.2.' + str(i))
+        disk_model = snmpget('1.3.6.1.4.1.6574.2.1.1.3.' + str(i))
+        disk_type = snmpget('1.3.6.1.4.1.6574.2.1.1.4.' + str(i))
         disk_status_nr = snmpget('1.3.6.1.4.1.6574.2.1.1.5.' + str(i))
         disk_temp = snmpget('1.3.6.1.4.1.6574.2.1.1.6.' + str(i))
         status_translation = {
@@ -105,7 +178,7 @@ if mode == 'disk':
             '5': "Crashed"
         }
         disk_status = status_translation.get(disk_status_nr)
-        disk_name = disk_name.replace(" ", "")
+        #disk_name = disk_name.replace(" ", "")
 
         if warning and warning < int(disk_temp):
             if state != 'CRITICAL':
@@ -113,16 +186,19 @@ if mode == 'disk':
         if critical and critical < int(disk_temp) or int(disk_status_nr) == (4 or 5):
             state = 'CRITICAL'
 
-        output += ' - ' + disk_name + ': Status: ' + disk_status + ', Temperature: ' + disk_temp + ' C'
-        perfdata += 'temperature' + disk_name + '=' + disk_temp + 'c '
-    print '%s%s %s' % (state, output, perfdata)
+        output += ' - ' + disk_name + ': Status: ' + disk_status + ', Temperature: ' + disk_temp + 'C' + 'ID ' +  disk_id + ' Model ' + disk_model + ' Type ' + disk_type
+        #Remove spaces from disk name for performance data
+        disk_name = disk_name.replace(" ", "")
+        perfdata += ' temperature_' + disk_name + '=' + disk_temp + 'C '
+    print('%s %s %s' % (state, output, perfdata))
     exitCode()
 
 if mode == 'storage':
     output = ''
-    perfdata = '|'
-    for i in range(1,256,1):
-        storage_name = snmpget('1.3.6.1.2.1.25.2.3.1.3.' + str(i))
+    perfdata = '| '
+    for item in snmpwalk('1.3.6.1.2.1.25.2.3.1.3'):
+        i = item.oid.split('.')[-1]
+        storage_name = item.value
         if storage_name.startswith("/volume"):
             allocation_units = snmpget('1.3.6.1.2.1.25.2.3.1.4.' + str(i))
             size = snmpget('1.3.6.1.2.1.25.2.3.1.5.' + str(i))
@@ -139,12 +215,14 @@ if mode == 'storage':
             if critical and critical < int(storage_used_percent):
                 state = 'CRITICAL'
 
-            output += ' -  free space: ' + storage_name + ' ' + str(storage_free) + ' GB (' + str(storage_used) + ' GB von ' + str(storage_size) + ' GB belegt, ' + str(storage_used_percent) + '%)'
-            perfdata += storage_name + '=' + str(storage_used) + 'c '
-    print '%s%s %s' % (state, output, perfdata)
+            output += ' -  free space: ' + storage_name + ' ' + str(storage_free) + ' GB (' + str(storage_used) + ' GB of ' + str(storage_size) + ' GB used, ' + str(storage_used_percent) + '%)'
+            perfdata += storage_name + '=' + str(storage_used) + 'GB ' + 'Used=' + str(storage_used_percent) + '%'
+    print('%s %s %s' % (state, output, perfdata))
     exitCode()
 
 if mode == 'update':
+    output = ''
+    perfdata = '| '
     update_status_nr = snmpget('1.3.6.1.4.1.6574.1.5.4.0')
     update_dsm_verison = snmpget('1.3.6.1.4.1.6574.1.5.3.0')
     status_translation = {
@@ -161,14 +239,18 @@ if mode == 'update':
         state = 'CRITICAL'
 
     update_status = status_translation.get(update_status_nr)
-    print state + ' - DSM Version: %s, DSM Update: %s' % (update_dsm_verison, update_status), '| DSMupdate=%sc' % update_status_nr
+    output = (state + ' - DSM Version: %s, DSM Update: %s' % (update_dsm_verison, update_status))
+    perfdata += 'DSM_update={}'.format(update_status_nr)
+    print(output, perfdata)
     exitCode()
 
 if mode == 'status':
+    output = ''
+    perfdata = '| '
     status_model = snmpget('1.3.6.1.4.1.6574.1.5.1.0')
     status_serial = snmpget('1.3.6.1.4.1.6574.1.5.2.0')
-    status_temperature = snmpget('1.3.6.1.4.1.6574.1.2.0')
-    
+ #   status_temperature = snmpget('1.3.6.1.4.1.6574.1.2.0')
+
     status_system_nr = snmpget('1.3.6.1.4.1.6574.1.1.0')
     status_system_fan_nr = snmpget('1.3.6.1.4.1.6574.1.4.1.0')
     status_cpu_fan_nr = snmpget('1.3.6.1.4.1.6574.1.4.2.0')
@@ -184,10 +266,13 @@ if mode == 'status':
     status_cpu_fan = status_translation.get(status_cpu_fan_nr)
     status_power = status_translation.get(status_power_nr)
 
-    if warning and warning < int(status_temperature):
-        state = 'WARNING'
-    if critical and critical < int(status_temperature):
-        state = 'CRITICAL'
+  #  if warning and warning < int(status_temperature):
+  #      state = 'WARNING'
+  #  if critical and critical < int(status_temperature):
+  #      state = 'CRITICAL'
+  #  print(state + ' - Model: %s, S/N: %s, System Temperature: %s C, System Status: %s, System Fan: %s, CPU Fan: %s, Powersupply : %s' % (status_model, status_serial, status_temperature, status_system, status_system_fan, status_cpu_fan, status_power) )
+    output = (state + ' - Model: %s, S/N: %s, System Status: %s, System Fan: %s, CPU Fan: %s, Powersupply: %s' % (status_model, status_serial, status_system, status_system_fan, status_cpu_fan, status_power) )
+    print(output)
 
-    print state + ' - Model: %s, S/N: %s, System Temperature: %s C, System Status: %s, System Fan: %s, CPU Fan: %s, Powersupply : %s' % (status_model, status_serial, status_temperature, status_system, status_system_fan, status_cpu_fan, status_power) + ' | system_temp=%sc' % status_temperature
+          #+ ' | system_temp=%sc' % status_temperature)
     exitCode()
